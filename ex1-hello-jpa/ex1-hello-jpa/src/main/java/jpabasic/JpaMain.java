fa44998a425131ecfb184ff7c87595714fe8bc3a
allready-dawn <qwshpke@naver.com>
2024-01-20 21:32:19 +0900

    ex1 JPA 기본 셋팅

---
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/jpabasic/JpaMain.java
@@ -0,0 +1,28 @@
+package jpabasic;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+
+        Member member = new Member();
+        member.setId(100L);
+        member.setName("HelloJPA");
+
+
+        em.close();
+        emf.close();
+
+    }
+}
