561b8bc9ae647770b680be97569a124131665f76
allready-dawn <qwshpke@naver.com>
2024-01-30 02:19:23 +0900

    값 타입

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,67 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    private String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
+
+    public boolean isVaild() {
+
+        return false;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    private Address setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    private Address setStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private Address setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -11,12 +11,47 @@ public class Delivery extends BaseEntity {
     @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
+    public Long getId() {
+        return id;
+    }
+
+    public Delivery setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Delivery setAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public Delivery setStatus(DeliveryStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Delivery setOrder(Order order) {
+        this.order = order;
+        return this;
+    }
 }
